1436f38efddaca02d0ee6b37732f4ebfc707082d
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-07-08 14:13:48 +0200

    Initial commit

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/email/StartupBean.java
@@ -0,0 +1,180 @@
+/*
+﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.europa.ec.fisheries.uvms.plugins.email;
+
+import eu.europa.ec.fisheries.schema.exchange.plugin.types.v1.PluginType;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.*;
+import javax.jms.JMSException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.europa.ec.fisheries.schema.exchange.service.v1.CapabilityListType;
+import eu.europa.ec.fisheries.schema.exchange.service.v1.ServiceType;
+import eu.europa.ec.fisheries.schema.exchange.service.v1.SettingListType;
+import eu.europa.ec.fisheries.uvms.exchange.model.constant.ExchangeModelConstants;
+import eu.europa.ec.fisheries.uvms.exchange.model.exception.ExchangeModelMarshallException;
+import eu.europa.ec.fisheries.uvms.exchange.model.mapper.ExchangeModuleRequestMapper;
+import eu.europa.ec.fisheries.uvms.plugins.email.mapper.ServiceMapper;
+import eu.europa.ec.fisheries.uvms.plugins.email.producer.PluginMessageProducer;
+import eu.europa.ec.fisheries.uvms.plugins.email.service.ExchangeService;
+import eu.europa.ec.fisheries.uvms.plugins.email.service.FileHandlerBean;
+
+@Singleton
+@Startup
+@DependsOn({"PluginMessageProducer", "FileHandlerBean", "PluginAckEventBusListener"})
+public class StartupBean extends PluginDataHolder {
+
+    final static Logger LOG = LoggerFactory.getLogger(StartupBean.class);
+
+    private final static int MAX_NUMBER_OF_TRIES = 10;
+    private boolean isRegistered = false;
+    private boolean isEnabled = false;
+    private boolean waitingForResponse = false;
+    private int numberOfTriesExecuted = 0;
+
+    @EJB
+    PluginMessageProducer messageProducer;
+
+    @EJB
+    ExchangeService service;
+
+    @EJB
+    FileHandlerBean fileHandler;
+
+    private CapabilityListType capabilities;
+    private SettingListType settingList;
+    private ServiceType serviceType;
+
+    @PostConstruct
+    public void startup() {
+
+        //This must be loaded first!!! Not doing that will end in dire problems later on!
+        super.setPluginApplicaitonProperties(fileHandler.getPropertiesFromFile(PluginDataHolder.PLUGIN_PROPERTIES));
+
+        //Theese can be loaded in any order
+        super.setPluginProperties(fileHandler.getPropertiesFromFile(PluginDataHolder.PROPERTIES));
+        super.setPluginCapabilities(fileHandler.getPropertiesFromFile(PluginDataHolder.CAPABILITIES));
+
+        ServiceMapper.mapToMapFromProperties(super.getSettings(), super.getPluginProperties(), getRegisterClassName());
+        ServiceMapper.mapToMapFromProperties(super.getCapabilities(), super.getPluginCapabilities(), null);
+
+        capabilities = ServiceMapper.getCapabilitiesListTypeFromMap(super.getCapabilities());
+        settingList = ServiceMapper.getSettingsListTypeFromMap(super.getSettings());
+
+        serviceType = ServiceMapper.getServiceType(
+                getRegisterClassName(),
+                getApplicaionName(),
+                "A good description for the email",
+                PluginType.SATELLITE_RECEIVER,
+                getPluginResponseSubscriptionName());
+
+        register();
+
+        LOG.info("PLUGIN STARTED");
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        unregister();
+    }
+
+    @Schedule(second = "*/30", minute = "*", hour = "*", persistent = false)
+    public void timeout(Timer timer) {
+        if (!waitingForResponse && !isRegistered && numberOfTriesExecuted < MAX_NUMBER_OF_TRIES) {
+            LOG.info(getRegisterClassName() + " is not registered, trying to register");
+            register();
+            numberOfTriesExecuted++;
+        }
+        if (isRegistered) {
+            LOG.info(getRegisterClassName() + " is registered. Cancelling timer.");
+            timer.cancel();
+        } else if(numberOfTriesExecuted >= MAX_NUMBER_OF_TRIES) {
+            LOG.info(getRegisterClassName() + " failed to register, maximum number of retries reached.");
+        }
+    }
+
+    private void register() {
+        LOG.info("Registering to Exchange Module");
+        setWaitingForResponse(true);
+        try {
+            String registerServiceRequest = ExchangeModuleRequestMapper.createRegisterServiceRequest(serviceType, capabilities, settingList);
+            String correlationId = messageProducer.sendEventBusMessage(registerServiceRequest, ExchangeModelConstants.EXCHANGE_REGISTER_SERVICE);
+        } catch (JMSException | ExchangeModelMarshallException e) {
+            LOG.error("Failed to send registration message to {}", ExchangeModelConstants.EXCHANGE_REGISTER_SERVICE);
+            setWaitingForResponse(false);
+        }
+
+    }
+
+    private void unregister() {
+        LOG.info("Unregistering from Exchange Module");
+        try {
+            String unregisterServiceRequest = ExchangeModuleRequestMapper.createUnregisterServiceRequest(serviceType);
+            String correlationId = messageProducer.sendEventBusMessage(unregisterServiceRequest, ExchangeModelConstants.EXCHANGE_REGISTER_SERVICE);
+        } catch (JMSException | ExchangeModelMarshallException e) {
+            LOG.error("Failed to send unregistration message to {}", ExchangeModelConstants.EXCHANGE_REGISTER_SERVICE);
+        }
+    }
+
+    public String getPluginResponseSubscriptionName() {
+        return getRegisterClassName() + getSetting("application.responseTopicName");
+    }
+
+    public String getResponseTopicMessageName() {
+        return getSetting("application.groupid");
+    }
+
+    public String getRegisterClassName() {
+        return getSetting("application.groupid");
+    }
+
+    public String getApplicaionName() {
+        return getSetting("application.name");
+    }
+
+    public String getSetting(String key) {
+        try {
+            return (String) super.getPluginApplicaitonProperties().get(key);
+        } catch (Exception e) {
+            LOG.error("Failed to getSetting for key: " + key, getRegisterClassName());
+            return null;
+        }
+    }
+
+    public boolean isWaitingForResponse() {
+        return waitingForResponse;
+    }
+
+    public void setWaitingForResponse(boolean waitingForResponse) {
+        this.waitingForResponse = waitingForResponse;
+    }
+
+    public boolean isIsRegistered() {
+        return isRegistered;
+    }
+
+    public void setIsRegistered(boolean isRegistered) {
+        this.isRegistered = isRegistered;
+    }
+
+    public boolean isIsEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+}
---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/email/service/EmailService.java
@@ -0,0 +1,77 @@
+/*
+﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.europa.ec.fisheries.uvms.plugins.email.service;
+
+import eu.europa.ec.fisheries.schema.exchange.plugin.types.v1.EmailType;
+import eu.europa.ec.fisheries.uvms.plugins.email.exception.PluginException;
+import javax.annotation.Resource;
+import javax.ejb.Asynchronous;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ **/
+@Stateless
+@LocalBean
+public class EmailService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmailService.class);
+
+    @Resource(mappedName = "java:jboss/mail/gmail")
+    Session gmailSession;
+
+    /**
+     *
+     * @param email
+     * @throws
+     * eu.europa.ec.fisheries.uvms.plugins.email.exception.PluginException
+     */
+    //@Asynchronous
+    public void sendEmail(EmailType email) throws PluginException {
+
+        LOG.info("Sending Email from " + email.getTo() + " to " + email.getTo() + " : " + email.getSubject());
+
+        try {
+
+            MimeMessage message = new MimeMessage(gmailSession);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getTo()));
+
+            if (email.getCc() != null) {
+
+                for (String cc : email.getCc()) {
+                    Address adress = new InternetAddress(cc);
+                    message.addRecipient(Message.RecipientType.CC, adress);
+                }
+            }
+
+            message.setSubject(email.getSubject());
+            message.setContent(email.getBody(), "text/html; charset=utf-8");
+            
+            Transport.send(message);
+
+            LOG.info("Email was sent");
+
+        } catch (MessagingException e) {
+            LOG.error("Error while sending email : ", e);
+        }
+    }
+
+}
